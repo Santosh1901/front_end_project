87b7153100e0938631ec2c84a21069813460c48f
P V Santosh <pvsantosh2019@gmail.com>
2020-09-24 00:06:50 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const tl = gsap.timeline({ default: { ease: 'power1.out' } });
+const timelin = gsap.timeline({ default: { ease: 'power1.out' } });
 
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to('.slider', { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+timelin.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+timelin.to('.slider', { y: "-100%", duration: 1.5, delay: 0.5 });
+timelin.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+timelin.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+timelin.fromTo(".sec-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
